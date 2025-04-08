3509603b00c3b7b4a7ac16223e5a5d0bcdf81a5c
KwakDADA <down022@gmail.com>
2025-04-08 19:27:15 +0900

    🎨 UI: 커스텀 옵션 버튼 구현
    - UIButton을 상속하는 class를 새로 정의
    - init 시 title을 입력

---
+++ Kiosk/Kiosk/Presentation/MovieInfo/MovieInfoConstant.swift
@@ -0,0 +1,41 @@
+//
+//  MovieInfoConstant.swift
+//  Kiosk
+//
+//  Created by 곽다은 on 4/8/25.
+//
+
+import Foundation
+import UIKit
+
+enum MovieInfoConstant {
+    enum Title {
+        static let title = "제목"
+        static let genre = "장르"
+        static let date = "상영 시간"
+        static let actor = "배우"
+        static let addButton = "추가"
+    }
+
+    enum Spacing {
+        static let extraSmall: CGFloat = 8
+        static let small: CGFloat = 12
+        static let medium: CGFloat = 20
+        static let large: CGFloat = 30
+    }
+
+    enum Config {
+        static let titleLine = 1
+        static let contentLine = 2
+        static let synopsisLine = 4
+    }
+
+    enum Ratio {
+        static let half = 0.5
+    }
+
+    enum ButtonImageName {
+        static let normalImage = "unselectedCircleButton"
+        static let selectedImage = "selectedCircleButton"
+    }
+}
