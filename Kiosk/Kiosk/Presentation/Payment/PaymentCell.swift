fbbf18bd0798762c4a57cc891be84a31e19e7a86
Hakyung Sohn <hortenssiaa@gmail.com>
2025-04-09 10:04:40 +0900

    ✨ Feat: PaymentCell 구현 1. 장바구니 총 결제금액   a. 로 장바구니에 들어간 항목에 대한 전체 금액 Lable 추가 2.  버튼 3.  버튼

---
+++ Kiosk/Kiosk/Presentation/Payment/PaymentCell.swift
@@ -0,0 +1,82 @@
+//
+//  PaymentCell.swift
+//  Kiosk
+//
+//  Created by kingj on 4/9/25.
+//
+
+import UIKit
+
+final class PaymentCell: UICollectionViewCell {
+    
+    let paymentLable = UILabel().then {
+        $0.text = PaymentConstant.paymentLabel
+        $0.font = Common.FontStyle.subTitle
+        $0.textColor = .kioskWhite
+    }
+
+    let totalPaymentLabel = UILabel().then {
+        $0.text = "₩230,000"
+        $0.font = Common.FontStyle.subTitle
+        $0.textColor = .kioskWhite
+    }
+
+    let hStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.distribution = .fillEqually
+        $0.spacing = 20
+    }
+
+    let deleteButton = UIButton().then {
+        $0.setTitle(PaymentConstant.delete, for: .normal)
+        $0.setTitleColor(.kioskWhite, for: .normal)
+        $0.titleLabel?.font = Common.FontStyle.buttonTitle
+        $0.backgroundColor = .kioskRed
+        $0.layer.cornerRadius = Common.Config.buttonCornerRadius
+    }
+
+    let payButton = UIButton().then {
+        $0.setTitle(PaymentConstant.pay, for: .normal)
+        $0.setTitleColor(.kioskWhite, for: .normal)
+        $0.titleLabel?.font = Common.FontStyle.buttonTitle
+        $0.backgroundColor = .kioskBlue
+        $0.layer.cornerRadius = Common.Config.buttonCornerRadius
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureSubview()
+        configureAutoLayout()
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func configureSubview() {
+        [paymentLable, totalPaymentLabel, hStackView]
+            .forEach { addSubview($0) }
+
+        [deleteButton, payButton]
+            .forEach { hStackView.addArrangedSubview($0) }
+    }
+
+    private func configureAutoLayout() {
+        paymentLable.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview().offset(Common.Config.defaultSpacing)
+        }
+
+        totalPaymentLabel.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.trailing.equalToSuperview().offset(Common.Config.defaultSpacing)
+        }
+
+        hStackView.snp.makeConstraints {
+            $0.top.equalTo(paymentLable.snp.bottom).offset(25)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(50)
+        }
+    }
+}
