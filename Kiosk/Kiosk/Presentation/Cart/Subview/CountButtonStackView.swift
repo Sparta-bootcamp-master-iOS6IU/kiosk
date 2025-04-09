2ae05aaced417dd9a22264c6185b6e1c944f1c3e
JISUNG PARK <meowbutlerdev@gmail.com>
2025-04-09 19:12:23 +0900

    ✨ Feat: 티켓 갯수 증가/감소 버튼 액션 및 Delegate 구현

--- Kiosk/Kiosk/Presentation/Cart/Subview/CountButtonStackView.swift
+++ Kiosk/Kiosk/Presentation/Cart/Subview/CountButtonStackView.swift
@@ -3,6 +3,8 @@ import UIKit
 final class CountButtonStackView: UIStackView {
     private typealias Button = CartConstant.Button
 
+    weak var delegate: CountButtonStackViewDelegate?
+
     override init(frame: CGRect) {
         super.init(frame: frame)
 
@@ -14,8 +16,8 @@ final class CountButtonStackView: UIStackView {
     }
 
     private func configureUI() {
-        let increaseButton = createButton(image: Button.plus, color: .kioskBlue)
-        let decreaseButton = createButton(image: Button.minus, color: .kioskRed)
+        let increaseButton = createButton(image: Button.plus, color: .kioskBlue, action: #selector(didTapIncrease))
+        let decreaseButton = createButton(image: Button.minus, color: .kioskRed, action: #selector(didTapDecrease))
 
         axis = .vertical
         distribution = .fillEqually
@@ -24,7 +26,7 @@ final class CountButtonStackView: UIStackView {
             .forEach { addArrangedSubview($0) }
     }
 
-    private func createButton(image: String, color: UIColor) -> UIButton {
+    private func createButton(image: String, color: UIColor, action: Selector) -> UIButton {
         let config = UIImage.SymbolConfiguration(pointSize: Button.symbolSize, weight: .bold)
 
         let image = UIImage(systemName: image, withConfiguration: config)
@@ -33,7 +35,16 @@ final class CountButtonStackView: UIStackView {
         button.setImage(image, for: .normal)
         button.tintColor = .kioskWhite
         button.backgroundColor = color
+        button.addTarget(self, action: action, for: .touchUpInside)
 
         return button
+    }
+
+    @objc private func didTapIncrease() {
+        delegate?.didTapIncrease()
+    }
+
+    @objc private func didTapDecrease() {
+        delegate?.didTapDecrease()
     }
 }
